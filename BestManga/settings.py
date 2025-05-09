e1e589783961f8db4dd2f057fa2de2bf7f07c2f9
MaksDie001 <anonrmv@gmail.com>
2025-05-09 16:27:52 +0500

    renderFix

--- BestManga/settings.py
+++ BestManga/settings.py
@@ -57,6 +57,7 @@ SITE_ID = 1  # Необходимо для работы allauth
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -168,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 # Default primary key field type
